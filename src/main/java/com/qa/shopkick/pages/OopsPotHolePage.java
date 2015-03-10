f3cb22fdbb2503233b18d0daf5ca8ce121c4e6bd
Vivek Nyayapathi <vivekn@shopkick.com>
2015-03-10 14:26:50 -0700

    changed to mobile element 'since dialog box is android only'

--- src/main/java/com/qa/shopkick/pages/OopsPotHolePage.java
+++ src/main/java/com/qa/shopkick/pages/OopsPotHolePage.java
@@ -1,8 +1,10 @@
 package com.qa.shopkick.pages;
 
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
@@ -11,9 +13,10 @@ import com.qa.shopkick.utils.AbstractPage;
 public class OopsPotHolePage extends AbstractPage{
 	
 	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"android:id/button1\").text(\"OK\")")
-	private static WebElement errorMessageOkButton;
+	private static WebElement errorMessageOkButtonWebElement;
 	
 	public static void clickOnOkToDismissError(){
+		MobileElement errorMessageOkButton = (MobileElement) driver.findElement(By.name("OK"));
 		errorMessageOkButton.click();
 		PageFactory.initElements(new AppiumFieldDecorator(driver), new SignInPage());
 		PageFactory.initElements(new AppiumFieldDecorator(driver), new LandingPage());
