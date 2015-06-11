4c95cdfad37e8efb8d93931db5aff2075451e9db
Vivek Nyayapathi <vivekn@shopkick.com>
2015-06-11 15:37:45 -0700

    added new class for authentication tests

--- src/main/java/com/qa/shopkick/pages/LeftNavBar.java
+++ src/main/java/com/qa/shopkick/pages/LeftNavBar.java
@@ -25,7 +25,7 @@ public class LeftNavBar extends AbstractPage{
 	private static WebElement guestUserProfileHEader;
 
 	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/button_text\").text(\"SIGN IN WITH FACEBOOK\")")
-	@iOSFindBy(uiAutomator = ".buttons(1)[\"Sign in with Facebook\"]")
+	@iOSFindBy(xpath = "//UIAApplication[1]/UIAWindow[2]/UIATableView[1]/UIATableCell[1]/UIAButton[1]")
 	private static WebElement signInWithFacebookLeftNav;
 
 	public static void clickOnFaceBookSignIn(){
---
+++ src/test/java/com/qa/shopkick/tests/AuthenticationTests.java
@@ -0,0 +1,60 @@
+package com.qa.shopkick.tests;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import com.qa.shopkick.pages.CountryPickerPage;
+import com.qa.shopkick.pages.FirstUseDealsEducationPage;
+import com.qa.shopkick.pages.FirstUseRegistrationIntroPage;
+import com.qa.shopkick.pages.FirstUseRewardsEducationPage;
+import com.qa.shopkick.pages.FirstUseWalkinEducationPage;
+import com.qa.shopkick.pages.LandingPage;
+import com.qa.shopkick.pages.MicrophonePermissionPage;
+import com.qa.shopkick.pages.RewardsPickerPage;
+import com.qa.shopkick.pages.SignInPage;
+import com.qa.shopkick.utils.AbstractTest;
+import com.qa.shopkick.utils.CustomHooks;
+
+public class AuthenticationTests extends AbstractTest{
+	
+	@Test
+	public void FirstUseFlow(){
+		//Go through the first use flow
+		FirstUseDealsEducationPage.clickNextButton();
+		CustomHooks.waitFor(2);
+
+		if(PlatformType.equals("IOS")){
+			CountryPickerPage.clickCountryUSA();
+			CountryPickerPage.clickNextButton();
+		}
+		FirstUseWalkinEducationPage.clickNextButton();
+		FirstUseRewardsEducationPage.clickNextButton();
+
+		//Pick default reward
+		RewardsPickerPage.clickNextButton();
+	}
+	
+	@Test
+	public void LoginWithFacebook(){
+		if(!SignInPage.isFacebookLoginAvail() || FirstUseRegistrationIntroPage.isItLegacySignUpScreen())
+		{
+			FirstUseRegistrationIntroPage.clickSignUpOrSignInButton();
+		}
+		
+		SignInPage.clicksignInFacebookSignIn();
+
+		CustomHooks.waitFor(10);
+
+		CustomHooks.dismissMultiLoginMessage();
+
+		CustomHooks.waitFor(5);
+
+		if (driver.findElement(By.name("OK")).isDisplayed())
+		{
+			//if iOS and if mic permission is asked, give the permission
+			MicrophonePermissionPage.clickOKButton();
+		}
+		LandingPage.openLeftNavSignedIn();
+	}
+
+}
