0794c0ed87c8c5cd5585338adf21dffcdf6f434f
Vivek Nyayapathi <vivekn@shopkick.com>
2015-06-17 11:17:58 -0700

    added email authentication test suite

---
+++ src/test/java/com/qa/shopkick/tests/Authentication/EmailAuthentication.java
@@ -0,0 +1,130 @@
+package com.qa.shopkick.tests.Authentication;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+
+import com.qa.shopkick.pages.AccountSettings;
+import com.qa.shopkick.pages.ConfirmDeleteAccountPage;
+import com.qa.shopkick.pages.DeleteAccountPage;
+import com.qa.shopkick.pages.EmailSignInPage;
+import com.qa.shopkick.pages.LandingPage;
+import com.qa.shopkick.pages.LeftNavBar;
+import com.qa.shopkick.pages.LeftNavSettings;
+import com.qa.shopkick.pages.LinkPhonePage;
+import com.qa.shopkick.pages.SignInPage;
+import com.qa.shopkick.pages.VerifyMobileNumber;
+import com.qa.shopkick.utils.AbstractTest;
+import com.qa.shopkick.utils.CustomHooks;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class EmailAuthentication extends AbstractTest
+{
+	@Test
+	public void Test1_createEmailAccount()
+	{
+		//dismiss the beta env. error that we see during logout
+		CustomHooks.dismissPotHoleError();
+
+		//From logout screen hit on email to create an account
+		SignInPage.clicksignInEmailSignIn();
+
+		//CREATE ACCOUNT
+		//add email address
+		EmailSignInPage.clickAndEnterEmailANDROID("trest2@sk.com");
+
+		//add password
+		EmailSignInPage.clickAndEnterPswdANDROID("sktest123");
+
+		//confirm password
+		EmailSignInPage.enterConfirmPassword();
+
+		//Add first and last name
+		EmailSignInPage.clickAndEnterFirstName();
+		EmailSignInPage.clickAndEnterLastName();
+
+		//Hit sign up
+		EmailSignInPage.clickOnSignUpButton();
+
+		CustomHooks.waitFor(5);
+
+		//Phone number screen
+
+		//add phone number
+		LinkPhonePage.clickAndEnterPhoneNumber();
+
+		//Add zipcode
+		LinkPhonePage.clickAndEnterZipcode();
+
+		//hit continue
+		LinkPhonePage.clickOnCompleteRegistration();
+
+		CustomHooks.waitFor(10);
+
+		//Hit Not right now in verify phone number page
+		VerifyMobileNumber.clickNotNowForVerifyPhone();
+	}
+
+	@Test
+	public void Test2_DeleteEmailAccount(){
+		if(!driver.findElement(By.name("Your settings")).isDisplayed()){
+			//Go to left nav bar
+			LandingPage.openLeftNavSignedIn();
+
+			//Go to settings
+			LeftNavBar.clickOnSettingsLeftNav();
+
+		}
+
+		//Go to Account settings
+		LeftNavSettings.clicksettingsAccountSettings();
+
+		//Tap on delete account
+		AccountSettings.clickAccountSettingsDeleteAccount();
+
+		//In delete account tap on confirm delete account
+		DeleteAccountPage.clickAndDeleteAccountWithPassword();
+
+		CustomHooks.waitFor(3);
+		//Tap on top left corner to proceed with delete
+		DeleteAccountPage.clickToProceedDelete();
+
+		CustomHooks.waitFor(5);
+
+		//Comfirm delete account
+		ConfirmDeleteAccountPage.clickOnDeleteAccountConfirm();
+	}
+
+	@Test
+	public void Test3_LoginWithEmail(){
+		CustomHooks.dismissPotHoleError();
+
+		//User logs in via email
+		SignInPage.clicksignInEmailSignIn();
+
+		//Element issues, using two different methods to sign in
+
+		if (PlatformType.equalsIgnoreCase("Android")) {
+
+			//add email address
+			EmailSignInPage.clickAndEnterEmailANDROID("a@b.com");
+			//add password
+			EmailSignInPage.clickAndEnterPswdANDROID("aaaaaa");
+		}
+		else
+		{
+			EmailSignInPage.clickAndEnterEmailIOS("a@b.com");
+
+			EmailSignInPage.clickAndEnterPasswordIOS("aaaaaa");
+		}
+
+		CustomHooks.dismissMultiLoginMessage();
+
+	}
+
+	@Test
+	public void Test4_logOutOfEmail(){
+		CustomHooks.LogMeOut();
+	}
+}
