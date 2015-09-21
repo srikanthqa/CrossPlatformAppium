16b3939cc249821a19bbf05b30d7ea03ff63384c
Vivek Nyayapathi <vivekn@shopkick.com>
2015-09-21 16:11:44 -0700

    bug fix for typo on uiAutomator

--- src/main/java/com/qa/shopkick/pages/KicksActivityPage.java
+++ src/main/java/com/qa/shopkick/pages/KicksActivityPage.java
@@ -11,31 +11,31 @@ import org.openqa.selenium.support.PageFactory;
 
 public class KicksActivityPage extends AbstractPage {
 	
-	@AndroidFindBy(UiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/app_screen_header\")")
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/app_screen_header\")")
 	private static WebElement ScreenHeader;
 	
-	@AndroidFindBy(UiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/kicks_activity_available_kicks_count\")")
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/kicks_activity_available_kicks_count\")")
 	private static WebElement AvailableKicksCount;
 	
-	@AndroidFindBy(UiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/kicks_activity_available_kicks_label\")")
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/kicks_activity_available_kicks_label\")")
 	private static WebElement AvailableKicksLabel;
 	
-	@AndroidFindBy(UiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/kicks_activity_all_time_kicks_count\")")
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/kicks_activity_all_time_kicks_count\")")
 	private static WebElement AllTimeKicksCount;
 
-	@AndroidFindBy(UiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/kicks_activity_all_time_kicks_label\")")
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/kicks_activity_all_time_kicks_label\")")
 	private static WebElement AllTimeKicksLabel;
 	
-	@AndroidFindBy(UiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/kicks_activity_available_label\")")
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/kicks_activity_available_label\")")
 	private static WebElement AvailableLabel;
 	
-	@AndroidFindBy(UiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/kicks_activity_all_time_label\")")
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/kicks_activity_all_time_label\")")
 	private static WebElement AllTimeLabel;
 	
-	@AndroidFindBy(UiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/ka_pending_header\").text(\"Pending\")")
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/ka_pending_header\").text(\"Pending\")")
 	private static WebElement PendingHeader;
 	
-	@AndroidFindBy(UiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/ka_pending_header\").text(\"Past\")")
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/ka_pending_header\").text(\"Past\")")
 	private static WebElement PastHeader;
 	
 }
