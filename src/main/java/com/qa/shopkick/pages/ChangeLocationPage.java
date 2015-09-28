75cf952778b6048ae1e349e7705092596e85e11b
nrvivek23 <viveknyayapathi@Viveks-MacBook-Pro-3.local>
2015-09-28 13:44:13 -0700

    Added new pages for store search and store location

---
+++ src/main/java/com/qa/shopkick/pages/ChangeLocationPage.java
@@ -0,0 +1,59 @@
+package com.qa.shopkick.pages;
+
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import com.qa.shopkick.utils.AbstractPage;
+
+public class ChangeLocationPage extends AbstractPage{
+
+	public static String searchedCity;
+	public static String resultCity;
+
+	//search location
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/location_search_bar\")")
+	public static WebElement searchLocation;
+
+	public static void clickAndChangeLocation(String searchCity, String state) {
+		
+		StringBuilder searchLocationAndState = new StringBuilder();
+		searchLocationAndState.append(searchCity);
+		searchLocationAndState.append(", ");
+		searchLocationAndState.append(state);
+		
+		searchCity = searchLocationAndState.toString();
+		
+		searchLocation.click();
+		searchLocation.clear();
+		searchLocation.sendKeys(searchCity);
+	}
+
+	//current location
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/current_location\")")
+	public static WebElement currentLocationButton;
+	
+	public static void clickCurrentLocation()
+	{
+		currentLocationButton.click();
+	}
+	
+	//search location result item
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/location_name\")")
+	public static WebElement searchResultItem;
+	
+	public static void selectSearchresultItem()
+	{
+		resultCity = searchResultItem.getText();
+		if(searchedCity.equalsIgnoreCase(resultCity));
+		{
+			searchResultItem.click();
+			PageFactory.initElements(new AppiumFieldDecorator(driver),new StoresPage());
+			PageFactory.initElements(new AppiumFieldDecorator(driver),new StoreSearchPage());
+		}
+	}
+
+	
+}
---
+++ src/main/java/com/qa/shopkick/pages/StoreSearchPage.java
@@ -0,0 +1,66 @@
+package com.qa.shopkick.pages;
+
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import com.qa.shopkick.utils.AbstractPage;
+
+public class StoreSearchPage extends AbstractPage{
+	public static String searchedStore;
+
+	//press back to dismiss keyboard
+
+	//search bar
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/store_search_text_field\")")
+	public static WebElement searchViewSearchBar;
+
+	public static void clickOnSearchbarAndSearch(String storeName)
+	{
+		searchViewSearchBar.click();
+		searchViewSearchBar.clear();
+		searchViewSearchBar.sendKeys(storeName);
+
+		//assign to a global variable
+		searchedStore = storeName;
+	}
+
+	//Search results and clicking them
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/chain_name\")")
+	public static WebElement searchResult;
+
+	public static void selectSearchedStore()
+	{
+		//grab searchitem name
+		String searchResultItem = searchResult.getText();
+
+		//check the list item is same as the one user searched for
+		if((searchResultItem.equalsIgnoreCase(searchedStore)))
+		{
+			searchResult.click();
+			PageFactory.initElements(new AppiumFieldDecorator(driver),new StoreViewPage());
+		}
+	}
+	
+	//Location change button
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/choose_location\")")
+	public static WebElement changeLocation;
+	
+	public static void clickOnChangeLocation() {
+		changeLocation.click();
+		PageFactory.initElements(new AppiumFieldDecorator(driver),new ChangeLocationPage());
+	}
+
+	//close search bar
+	@AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopkick.app:id/clear_search\")")
+	public static WebElement clearSearchButton;
+	
+	public static void clickClearSearchButton(){
+		clearSearchButton.click();
+		PageFactory.initElements(new AppiumFieldDecorator(driver),new StoresPage());
+		
+	}
+
+}
