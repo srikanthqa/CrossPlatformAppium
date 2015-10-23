84b8907f83e15eaf7de82dfd2e698e9845ce5489
Manish Madhusoodan <mmadhusoodan@Manishs-MacBook-Pro.local>
2015-10-23 15:26:49 -0700

    Single Test working now

--- src/test/java/com/qa/shopkick/tests/Authentication/EmailAuthentication.java
+++ src/test/java/com/qa/shopkick/tests/Authentication/EmailAuthentication.java
@@ -3,13 +3,12 @@ package com.qa.shopkick.tests.Authentication;
 import com.qa.shopkick.pages.EmailSignInPage;
 import com.qa.shopkick.pages.SignInPage;
 import com.qa.shopkick.utils.AbstractTest;
-import com.qa.shopkick.utils.CustomHooks;
 import org.apache.log4j.Logger;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
-import static junit.framework.TestCase.assertEquals;
+import static com.thoughtworks.selenium.SeleneseTestCase.assertNotEquals;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 
@@ -17,37 +16,10 @@ public class EmailAuthentication extends AbstractTest {
     final private static Logger log = Logger.getLogger((EmailAuthentication.class));
     final private static String EXPECTED_ERROR = "Oh no, that email/password isn't right. Try again or tap to reset password.";
 
-    @Test
-    public void Test1_LoginWrongEmailAccount() {
-
-        testName = "Test1_LoginWrongEmailAccount";
-        //Go thorough first use
-        LoginHooks.GoThroughFirstUse();
-
-        //From logout screen hit on email to create an account
-        SignInPage.clicksignInEmailSignIn();
-
-        //CREATE ACCOUNT
-        //add email address
-        EmailSignInPage.clickAndEnterEmailANDROID("trest2@sk.com");
-
-        //add password
-        EmailSignInPage.clickAndEnterPswdANDROID("sktest123");
-
-        //Hit sign in
-        EmailSignInPage.clickLoginButton();
-
-        String ACTUAL_ERROR = EmailSignInPage.getEmailPasswordError();
-        assertEquals("ERROR! " + EXPECTED_ERROR, ACTUAL_ERROR, EXPECTED_ERROR);
-        CustomHooks.pressBack();
-        CustomHooks.pressBack();
-        runStatus = "passed";
-    }
-
 //    @Test
-//    public void Test2LoginWithEmailAccount() {
+//    public void Test1_LoginWrongEmailAccount() {
 //
-//        testName = "Test2LoginWithEmailAccount";
+//        testName = "Test1_LoginWrongEmailAccount";
 //        //Go thorough first use
 //        LoginHooks.GoThroughFirstUse();
 //
@@ -56,20 +28,47 @@ public class EmailAuthentication extends AbstractTest {
 //
 //        //CREATE ACCOUNT
 //        //add email address
-//        EmailSignInPage.clickAndEnterEmailANDROID("qa_auto1@gmail.com");
+//        EmailSignInPage.clickAndEnterEmailANDROID("trest2@sk.com");
 //
 //        //add password
-//        EmailSignInPage.clickAndEnterPswdANDROID("123456");
+//        EmailSignInPage.clickAndEnterPswdANDROID("sktest123");
 //
 //        //Hit sign in
 //        EmailSignInPage.clickLoginButton();
 //
 //        String ACTUAL_ERROR = EmailSignInPage.getEmailPasswordError();
-//        assertNotEquals(ACTUAL_ERROR, EXPECTED_ERROR);
-//
-//        //make sure you see the Kicks Count on the top center
+//        assertEquals("ERROR! " + EXPECTED_ERROR, ACTUAL_ERROR, EXPECTED_ERROR);
+//        CustomHooks.pressBack();
+//        CustomHooks.pressBack();
 //        runStatus = "passed";
 //    }
+
+    @Test
+    public void Test2LoginWithEmailAccount() {
+
+        testName = "Test2LoginWithEmailAccount";
+        //Go thorough first use
+        LoginHooks.GoThroughFirstUse();
+
+        //From logout screen hit on email to create an account
+        SignInPage.clicksignInEmailSignIn();
+
+        //CREATE ACCOUNT
+        //add email address
+        EmailSignInPage.clickAndEnterEmailANDROID("qa_auto1@gmail.com");
+
+        //add password
+        EmailSignInPage.clickAndEnterPswdANDROID("123456");
+
+        //Hit sign in
+        EmailSignInPage.clickLoginButton();
+
+        String ACTUAL_ERROR = EmailSignInPage.getEmailPasswordError();
+        assertNotEquals(ACTUAL_ERROR, EXPECTED_ERROR);
+
+        //make sure you see the Kicks Count on the top center
+        runStatus = "passed";
+    }
 
 
 /*    @Test
